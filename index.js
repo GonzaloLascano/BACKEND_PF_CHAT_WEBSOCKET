41de2437f7a314331d75d7fb888f6cea4d2a6ec1
Gonzalo Ivan Puniales Lascano <lascano.gonzalo.ivan@gmail.com>
2022-11-07 07:47:24 -0300

    Entrega del servidor chat con websocket para el Proyecto Final de Backend de CoderHouse

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express')
+const {SERVER} = require('./config/config')
 const { Server: HttpServer } = require('http')
 const { Server: IoServer} = require('socket.io')
-const { formValidation, productToDb, dbLoadProds, messageToDB, dbLoadMessages } = require('./modules/storage.js')
-let { products, messages } = require('./modules/storage.js')
-const { fakeList } = require('./mocks')
-
+const connectMong = require('./config/mongodb')
+const { messageHandler } = require('./services/messageService')
+const { logError, log, logWarn } = require('./config/log');
 
 /* ----------- SERVER -------------- */
 
@@ -13,59 +14,29 @@ const httpServer = new HttpServer(app)
 const io = new IoServer(httpServer) //Webockets Instance
 
 // Server set and running
-const PORT = 3000
 
-httpServer.listen(PORT, () => console.log('SERVER ON'))
-httpServer.on('error', (error) => console.log({mensaje: `hubo un error :( ${error}`}))
-app.use('/home', express.static(__dirname + '/public'))
-app.use('/api/productos-test', express.static(__dirname + '/test'))
-
+httpServer.listen(SERVER.PORT, async () => {
+  await connectMong();
+  log.info('SERVER RUNNING ON PORT', SERVER.PORT)
+})
+httpServer.on('error', (error) => logError.error(`hubo un error`, error))
+app.use('/', express.static(__dirname + '/public'))
 
 //sockets----------------
 
 io.on('connection', async (socket) => {
 
-  products = await dbLoadProds()
-  messages = await dbLoadMessages()
-  socket.emit('refreshProds', products)
-  socket.emit('refresh', messages)
-  socket.emit('mocks', fakeList)
-  console.log('Usuario conectado')
-  console.log(messages)
-
-
- // message sockets
+  log.info('User online!')
+  
+  socket.on('assistMe', async (data) => {
+    let messageHistory = await messageHandler(data)
+    messageHistory.error ? socket.emit('warning', messageHistory) :
+    socket.emit('refresh', messageHistory) 
+  })
 
   socket.on('message', async (data) =>{
-    let warning
-    let validation = formValidation(data)
-    if (validation){
-      messageToDB(data)
-      messages = await dbLoadMessages()
-      io.sockets.emit('refresh', messages)
-    }
-    else {
-      let warning = 'please submit a proper email address'
-      socket.emit('warning', warning)
-    }
-  })
-  
-  
- // product sockets
-
-  socket.on('newProd', async (data) =>{
-    //console.log(data)
-    let validation = formValidation(data)
-    if (validation) {
-        await productToDb(data)
-        products = await dbLoadProds()
-        io.sockets.emit('refreshProds', products)
-    }
-    else {
-      let warning = 'Price must be a number!'
-      socket.emit('warning', validation)
-    }
-      
+    let message = await messageHandler(data);
+    message.error ? socket.emit('warning', message) :
+    socket.emit('refresh', message) 
   })
 })  
-
--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,15 @@
 const socket = io.connect();
 
 function sendMsg() {
-    let message = {content: document.getElementById('content').value, date: Date(), user: document.getElementById('userMail').value}
+    let message = {body: document.getElementById('content').value, email: document.getElementById('userMail').value}
     socket.emit('message', message)
 }
 
-function sendProduct(){
-    let newProduct = { 
-        title: document.getElementById('prodTitle').value, 
-        price: document.getElementById('prodPrice').value,
-        thumbnail: document.getElementById('prodThumb').value
-    }
-    socket.emit('newProd', newProduct)
-}
-
-function handleRenderer(productList){
-    console.log(productList)
-    if (productList.length > 0) {
-        let tBody = document.getElementById('table_body')
-        tBody.innerHTML = ''
-        const template = Handlebars.compile(`
-        <th scope="row">{{id}}</th>
-        <td>{{title}}</td>
-        <td>$ {{price}}</td>
-        <td><img src={{img}}></td>
-        `)
-        
-        for (product of productList) {
-            console.log(product)
-            const html = template({id: product.id, title: product.title, price: product.price, img: product.thumbnail})
-            const tr = document.createElement('tr')
-            tr.innerHTML = html
-            tBody.appendChild(tr)
-        }
-    }
+function assistMe(){
+    let enviarBtn = document.getElementById('enviar-btn') 
+    enviarBtn.className = "my-4 btn-dark btn-lg w-50 mx-auto"
+    let message = {body: 'hola! necesito asistencia!', email: document.getElementById('userMail').value}
+    socket.emit('assistMe', message)
 }
 
 socket.on('refresh', (data) => {
@@ -42,22 +18,17 @@ socket.on('refresh', (data) => {
     for (message of data) { 
         let msg = document.createElement('p')
         msg.innerHTML = `
-            <span class="mail fw-bold text-primary">${message.user} </span>
-            <span class="date" style="color: brown;">${message.date} </span>
-            <span class="user fst-italic text-success">${message.content} </span>
+            <span class="mail fw-bold text-primary">${message.email} </span>
+            <span class="date" style="color: brown;">${message.createdAt} </span>
+            <span class="user fst-italic text-success">${message.body} </span>
         `
         msgContainer.appendChild(msg)
     }
 
 })
 
-socket.on('refreshProds', (data) => {
-    handleRenderer(data)
-    console.log(data)
-})
-
 socket.on('warning', (data) => {
     console.log(data)
-    let alertWarning = data
+    let alertWarning = data.error
     alert(alertWarning)
 })
